c96f2d791e9e3529f05a1b2a250f624beaafd8c8
horiyuko0512 <0512.hori.yuko@gmail.com>
2025-01-12 02:47:59 +0900

    Added Header

---
+++ frontend/app/matches/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+const MatchListPage = () => {
+  return (
+    <Header/>
+  );
+};
+
+export default MatchListPage;
---
+++ frontend/app/my/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+const MatchListPage = () => {
+  return (
+    <>
+      <Header/>
+      hello
+    </>
+  )
+};
+
+export default MatchListPage;
